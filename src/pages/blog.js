5bef7a690a15b39394307a6e4f42448e825bafd5
shoaib <shoaiblaghari9@gmail.com>
2021-06-28 21:25:05 -0700

    blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../style/blog.css'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
-import { Card } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWordpress } from '@fortawesome/free-brands-svg-icons'
+
+const StyledContainer = styled(Container)`
+&{
+  margin-top: 30vh;
+  text-align: center;
+  transform: translateY(-50%);
+}
+`
+
+const StyledIcon = styled(FontAwesomeIcon)`
+&{
+  font-size: 2em;
+  margin-top: 40%;
+}
+`
 
 const BlogPage = () => {
   return (
     < >
       <Layout pageTitle="Blog">
-        <Card className="text-center">
-          Coming soon...
-        </Card>
+        <StyledContainer>
+          <a href="https://www.shoaiblaghari.me" target='_blank' rel="noopener noreferrer" className="special">
+            <StyledIcon icon={faWordpress}/>
+          </a>
+        </StyledContainer>
       </Layout>
     </>
   )
